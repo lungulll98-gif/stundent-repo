9d25b069b12607b40d52658fe6e41ca800177585
Pusca Vasile <vasea-pusca@mail.ru>
2025-11-15 19:42:05 +0200

    Introducerea studentilor

--- src/Student.java
+++ src/Student.java
@@ -7,5 +7,14 @@ public class Student {
         System.out.print("Introduceti facultatea (Matematica / Informatica): ");
         String alege = scanner.next();
 
+        System.out.print("Doriti sa introduceti studenti noi? (Da/Nu): ");
+        String raspuns = scanner.next();
+        if (raspuns.equalsIgnoreCase("Nu")){
+            System.out.println("Iesire din program...");
+            return;
+        }
+        System.out.print("Creati  numele grupei noi: ");
+        String grupa = scanner.next();
+        System.out.println("Introducerea studentilor grupei "+ grupa);
     }
 }
