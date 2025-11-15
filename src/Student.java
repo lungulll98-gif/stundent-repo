0ec9604fab3747b76e3206957c6d1ef87871bdc4
Lungu Victor <lungulll98@gmail.com>
2025-11-15 12:35:19 +0200

    Add introducere

---
+++ src/Student.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Student {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Introduceti facultatea (Matematica / Informatica): ");
+        String alege = scanner.next();
+
+    }
+}
